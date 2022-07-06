5db73acca58c7ccb60e017f87d3f9b8eb2e89682
zelihauslu <zelusluz@gmail.com>
2022-07-06 12:58:12 +0300

    ViewModel created

---
+++ Gallery/Gallery/ViewModels/ViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  ViewModel.swift
+//  Gallery
+//
+//  Created by Zeliha Uslu on 6.07.2022.
+//
+
+import SwiftUI
+
+struct ViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewModel()
+    }
+}
