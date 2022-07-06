5865e0909430a2fb3cbed813d2b6dec28503e7d5
zelihauslu <zelusluz@gmail.com>
2022-07-06 14:11:33 +0300

    Picker error enum

--- Gallery/Gallery/Models/Picker.swift
+++ Gallery/Gallery/Models/Picker.swift
@@ -6,18 +6,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 enum Picker {
     enum Source: String{
         case library, camera
     }
     
-    static func checkPermissions() -> Bool {
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            return true
-        } else {
-            return false
+    enum PickerError: Error, LocalizedError {
+        case unavailable
+        case restricted
+        case denied
+        
+        var errorDescription: String? {
+            switch self {
+            case .unavailable:
+                return NSLocalizedString("There is no available camera on this device", comment: "")
+            case .restricted:
+                return NSLocalizedString("You are not allowed to access media capture devices.", comment: "")
+            case .denied:
+                return NSLocalizedString("You have explicitly denied permission for media capture. Please open permissions/Privacy/Camera and grant access for this application.", comment: "")
+            }
         }
     }
-}
-
+    
+    static func checkPermissions() throws {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let authStatus = AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
+            switch authStatus {
+            case .denied:
+                throw PickerError.denied
+            case .restricted:
+                throw PickerError.restricted
+            default:
+                break
+            }
+        } else {
+            throw PickerError.unavailable
+        }
+    }
