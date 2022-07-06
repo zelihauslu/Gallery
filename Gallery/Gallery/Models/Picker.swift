ed5e5d4d6443f3f416a5f7de7577c4b5687561cf
zelihauslu <zelusluz@gmail.com>
2022-07-06 13:24:24 +0300

    Picker model created

---
+++ Gallery/Gallery/Models/Picker.swift
@@ -0,0 +1,15 @@
+//
+//  Picker.swift
+//  Gallery
+//
+//  Created by Zeliha Uslu on 6.07.2022.
+//
+
+import Foundation
+
+enum Picker {
+    enum Source: String{
+        case library, camera
+    }
+}
+
